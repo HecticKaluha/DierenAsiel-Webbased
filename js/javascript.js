42ac0cf86c0459ad9949ce0198292c19b953bab2
Stefan Verhoeve <S.verhoeve@student.fontys.nl>
2015-11-17 11:14:40 +0100

    fix

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,17 @@
 // JavaScript Document
-function showp()
+function focus()
 {
 	$('form p').removeClass('hidden');	
 }
-function hidep()
+function blur()
 {
 	$('form p').addClass('hidden');	
 }
+function OnReady() 
+{
+ 	console.log('Pagina geladen, DOM klaar voor gebruik.');
+	$('#naam').focus(focus);
+	$('#naam').blur(blur);
+}
+
+$(document).ready(OnReady);
