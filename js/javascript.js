32eea38ad4a85187c50a04bbab9e8058978ffa12
Stefan Verhoeve <S.verhoeve@student.fontys.nl>
2015-11-20 15:55:22 +0100

    javascript local storage en meldingen bij form

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,13 @@
 // JavaScript Document
 function focus()
 {
-	$('form p').removeClass('hidden');	
+	$('#namelen').removeClass('hidden');	
 }
 function blur()
 {
-	$('form p').addClass('hidden');	
+	$('#regnr').addClass('hidden');
+	$('#namelen').addClass('hidden');
+	$('#age').addClass('hidden');
 }
 function checkform()
 {
@@ -17,44 +19,83 @@ function OnReady()
 	$('#naam').focus(focus);
 	$('#naam').blur(blur);
 	$('#btn').click(OnSubmit);
+	/*if(localStorage.getItem( 'Kat' ) != null)
+	{
+		window.alert(localStorage.getItem( 'Kat' ));
+	}
+	if(localStorage.getItem( 'Hond' ) != null)
+	{
+		window.alert(localStorage.getItem( 'Hond' ));
+	}*/
+	var animal_list = JSON.parse(localStorage.getItem('animal_list'));
+	if (!Array.isArray(animal_list))
+	{
+  		animal_list = [];
+	}
+	else
+	{
+		for (var i = 0; i < animal_list.length; ++i) 
+		{
+			var animal = animal_list[i];
+			window.alert(animal);
+  		}
+	}
 }
 
 $(document).ready(OnReady);
 
 function OnSubmit(event) 
 {
-	var animal = 
-	{
-	  'species':  $('input[name=type]:checked').val(),
-	  'name':     $('#naam').val(),
-	  'age':      $('#leeftijd').val(),
-	  'regnr':    $('#registrationnumber').val(),
-	  'reserved': $('#status').is(':checked')
-	};
-	//fix!!!!1
-	if(animal.name.length == 0)
+	var animal_list = JSON.parse(localStorage.getItem('animal_list'));
+	blur();
+	var animal= {}; 
+	  animal.species =  $('input[name=type]:checked').val();
+	  animal.name=     $('#naam').val();
+	  animal.age=      $('#leeftijd').val();
+	  animal.regnr=   $('#registrationnumber').val();
+	  animal.reserved= $('#status').is(':checked');
+	  
+	if(animal.regnr.substring(0, 1) != "0" && animal.regnr != "")
 	{
 		event.preventDefault();
-		console.log('Het formulier is niet verzonden. De naam heeft 0 karakters');
-		focus();
+		$('#regnr').removeClass('hidden');
 	}
-	if(animal.age != 'undefined' && animal.age <= 0)
+	else if(animal.name.length < 1)
 	{
 		event.preventDefault();
-		console.log('De leeftijd van het dier moet groter zijn dan 0');
-				console.log(animal.name);
+		$('#namelen').removeClass('hidden');
 	}
-	if(animal.regnr != 'undefined' && animal.regnr.substring(0, 1) != 0)
+	else if(animal.age != 'undefined' && animal.age < 1)
 	{
 		event.preventDefault();
-		console.log('Het eerste nummer van hetregistrationnummer moet gelijk zijn aan 0');
-		//insert message
+		$('#age').removeClass('hidden');
 	}
 	else
 	{
-		console.log('Het formulier is verzonden. De naam heeft ' + animal.name.length + ' karakters');
 		blur();
-	}	
-	//Fix!!!!	
+	}
+	if(animal.species == "Kat")
+	{
+		var Kat = JSON.stringify(animal); //Kat data omzetten naar string values
+		localStorage.setItem('Kat', Kat); //Item toevoegen aan local storage
+		console.log(Kat);
+
+		var dier = JSON.parse( localStorage.getItem( 'Kat' ) ); //van string object maken
+		console.log(dier);
+		animal_list.push(dier);
+		window.alert("Het dier is een kat!");
+		console.log(dier);
+	}
+		if(animal.species == "Hond")
+	{
+		var Hond = JSON.stringify(animal);
+		localStorage.setItem('Hond', Hond);
+		
+		var dier = JSON.parse( localStorage.getItem( 'Hond' ) );
+		console.log(dier.name);
+		animal_list.push(dier);
+		window.alert("het dier is een Hond!");
+	}
+	localStorage.setItem('animal_List', JSON.stringify(animal_list));
 }
 
